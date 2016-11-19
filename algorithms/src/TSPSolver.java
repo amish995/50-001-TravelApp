0f965b1fed1c5c7546f6ce2379f5ede7c902d557
jonbeibeibei <jonathanbeiqiyang@gmail.com>
2016-11-19 21:02:34 +0800

    working brute force algorithm implemented

--- algorithms/src/TSPSolver.java
+++ algorithms/src/TSPSolver.java
@@ -9,17 +9,31 @@ public class TSPSolver {
 
 
 
+
     public static void main(String[] args) {
         double budget = 40;
         TSPBruteforce tsp = new TSPBruteforce();
-        ArrayList<String> temp = new ArrayList<String>(Arrays.asList("Zoo","Buddha Tooth Relic Temple","Vivo City","Resorts World Sentosa","Singapore Flyer"));
+        ArrayList<String> temp = new ArrayList<String>(Arrays.asList("Resorts World Sentosa","Singapore Flyer"));
+        temp.add("Marina Bay Sands");
         ArrayList<ArrayList<String>> permutationList = new ArrayList<ArrayList<String>>();
+        ArrayList<ArrayList<String>> permutationList1 = new ArrayList<ArrayList<String>>();
         ArrayList<String> soln = new ArrayList<String>();
-
+        ArrayList<String> soln2 = new ArrayList<String>();
         permutationList = tsp.createPermutations(temp);
-        System.out.println(permutationList);
-        soln = tsp.getOptimalPath(permutationList);
+        permutationList1 = tsp.checkPermutations(permutationList);
+        soln = tsp.getOptimalPath(permutationList1);
+        System.out.println("solution" + soln);
+
+
+        ArrayList<ArrayList<int[]>>transportationModesMap = tsp.chooseTransport(soln, 300.0);
         System.out.println(soln);
+        System.out.println(Arrays.toString(transportationModesMap.get(0).get(0)));
+        int[] finalMode = transportationModesMap.get(0).get(0);
+
+        System.out.println(Arrays.toString(finalMode));
+        //calcalate the cost
+        System.out.println(tsp.getTotalCost(finalMode,soln));
+
 
 
 
